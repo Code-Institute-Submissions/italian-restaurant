320be7142f9d46bf47dd4093182fa1dbafee7194
joanavrsilva <joanavrsilva@gmail.com>
2022-02-25 23:58:04 +0000

    Create database model

--- reservations/models.py
+++ reservations/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
+
+class Clients(models.Model):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    email = models.EmailField()
+    phone_number = models.IntegerField()
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="reservations_management")
+
+class Reservation(models.Model):
+    date_time = models.DateTimeField()
+    number_of_people = models.IntegerField()
+    coment = models.TextField(max_length=100)
 
 # Create your models here.
