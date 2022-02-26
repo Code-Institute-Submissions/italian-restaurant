3ea63e3b3f71be27f7e49d664fa7a7cdd7864461
joanavrsilva <joanavrsilva@gmail.com>
2022-02-26 00:59:08 +0000

    Add new fields to Client model

--- reservations/models.py
+++ reservations/models.py
@@ -2,11 +2,14 @@ from django.db import models
 from django.contrib.auth.models import User
 from cloudinary.models import CloudinaryField
 
-class Clients(models.Model):
+class Client(models.Model):
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
     email = models.EmailField()
-    phone_number = models.IntegerField()
+    phone_number = models.CharField(max_length=30)
+    date_time = models.DateTimeField()
+    number_of_people = models.IntegerField(default=1)
+    coment = models.TextField(max_length=100)
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="reservations_management")
 
 class Reservation(models.Model):
